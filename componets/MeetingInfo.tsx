6c9e72afe18b9ed48bf1356914e5a1625e7cdc2e
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-20 10:17:56 -0400

    created component meetinginfo for amorphis date logging

---
+++ componets/MeetingInfo.tsx
@@ -0,0 +1,18 @@
+import { Meeting } from "@/types/type";
+
+
+export default function MeetingInfo({ meetings }: { meetings: Meeting[] }) {
+  return (
+    <div className="text-center space-y-2">
+      <h2 className="text-2xl font-bold text-blue-700">
+        Regular Season Meetings
+      </h2>
+
+      {meetings.map((meeting, i) => (
+        <p key={i}>
+          {meeting.date} — {meeting.type} | {meeting.time}
+        </p>
+      ))}
+    </div>
+  );
+}

09db4daa5e9c730feb620a71caee0132b3c8d38b
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-21 12:12:05 -0400

    set up the about page structure

---
+++ app/aboutUs/page.tsx
@@ -0,0 +1,15 @@
+export default function AboutUs() {
+  return (
+    <>
+    <div className="bg-gradient-to-b from-black via-blue-950 to-black min-h-screen">
+      <section className="flex justify-center px-4 py-16">
+        <div className="w-full max-w-4xl">
+          {/* ONE MAIN CARD */}
+          {/* sofen car up with "rounded-3xl" */}
+          <div className="bg-white text-gray-900 shadow-2xl p-8 space-y-10"></div>
+        </div>
+      </section>
+    </div>
+    </>
+  );
+}

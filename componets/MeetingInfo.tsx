f990d034b78a1b5210495a3686bf57de8000134a
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-22 11:37:10 -0400

    changed all instances of blue to red

--- componets/MeetingInfo.tsx
+++ componets/MeetingInfo.tsx
@@ -4,7 +4,7 @@ import { Meeting } from "@/types/type";
 export default function MeetingInfo({ meetings }: { meetings: Meeting[] }) {
   return (
     <div className="text-center space-y-2">
-      <h2 className="text-2xl font-bold text-blue-700">
+      <h2 className="text-2xl font-bold text-red-700">
         Regular Season Meetings
       </h2>
 

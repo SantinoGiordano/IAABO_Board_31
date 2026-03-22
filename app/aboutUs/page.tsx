52b2f6a5afe4631bd04ae60cafcd150295f3c9d0
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-22 14:19:26 -0400

    cleaned up comments, added new map for western mass

--- app/aboutUs/page.tsx
+++ app/aboutUs/page.tsx
@@ -1,19 +1,17 @@
 export default function AboutUs() {
   return (
     <>
-      {/* HERO SECTION */}
-      <section className="relative h-[60vh] bg-[url('/sicsDriveToTheBasket.avif')] bg-center bg-cover flex items-center justify-center md:bg-fixed"></section>
+      <section className="relative h-[60vh] bg-[url('/whiteAndRedDrive.jpg')] bg-center bg-cover flex items-center justify-center md:bg-fixed"></section>
 
-      {/* MAIN BACKGROUND AND CARD */}
       <div className=" min-h-screen flex justify-center px-4 py-16">
         <div className="w-full max-w-4xl">
           <div className="bg-white text-gray-900 shadow-2xl p-8 space-y-10">
-            {/* PAGE HEADING */}
+          
             <div className="text-center space-y-3">
-              <h2 className="text-3xl md:text-4xl font-bold text-blue-700">
+              <h2 className="text-3xl md:text-4xl font-bold text-red-700">
                 About Us
               </h2>
-              <p className="text-gray-600 text-lg leading-relaxed">
+              <div className="text-gray-600 text-lg leading-relaxed">
                 <p>
                   The Western Massachusetts Board of Approved Basketball
                   Officials&nbsp;is affiliated with the International
@@ -33,9 +31,19 @@ export default function AboutUs() {
                   beyond. Past members of Board 31 have gone on to work&nbsp;the
                   NCAA Division I men’s tournament and NBA G League.
                 </p>
-              </p>
+              </div>
             </div>
 
+            {/* Second option for the map, if we want to do something more custom than the google maps embed: */}
+            {/* <img src="/westernmassMap.jpg" alt="Board 31 Group Photo" className="w-full rounded-lg shadow-lg" /> */}
+
+            <div className="w-full rounded-lg overflow-hidden shadow-lg hover:scale-[1.01] transition">
+              <iframe
+                src="https://www.google.com/maps?q=Hampden+County+Massachusetts&output=embed"
+                className="w-full h-[400px] md:h-[500px] border-0"
+                loading="lazy"
+              ></iframe>
+            </div>
 
           </div>
         </div>

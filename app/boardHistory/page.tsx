e74f576649718725e57603f78ea343a0cb7ef786
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-31 15:13:26 -0400

    simple text section

--- app/boardHistory/page.tsx
+++ app/boardHistory/page.tsx
@@ -1,24 +1,33 @@
 import { DataTable } from "@/componets/tables";
-import { presidents, awardWinners, walterHicksonAward  } from "../../data/boardHistory";
+import {
+  presidents,
+  awardWinners,
+  walterHicksonAward,
+} from "../../data/boardHistory";
 
 export default function BoardHistory() {
   return (
     <div className="bg-black min-h-screen p-6 flex justify-center">
       <div className="w-full max-w-3xl">
-
         <h1 className="text-3xl font-bold text-white mb-10 text-center">
           Board History
         </h1>
 
         {/* TABLE 1 */}
         <DataTable title="Past Presidents" data={presidents} />
+        
+        <h4 className="text-white text-justify p-5">
+          Honoring a former board president and longtime rules interpreter, this
+          is presented to a member who has demonstrated exceptional officiating
+          ability and/or elevated his/her officiating to an elite level worthy
+          of recognition.
+        </h4>
 
         {/* TABLE 2 */}
         <DataTable title="Robert D'Agostino Award" data={awardWinners} />
 
         {/* TABLE 3 */}
         <DataTable title="Tom Award Winners" data={walterHicksonAward} />
-
       </div>
     </div>
   );

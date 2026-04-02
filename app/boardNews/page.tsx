35fd4cf11354605138a01fcee082413cbfd48478
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-02 11:05:29 -0400

    set up for board new

---
+++ app/boardNews/page.tsx
@@ -0,0 +1,12 @@
+export default function BoardNews() {
+    return (
+        <>
+        <div className="bg-black min-h-screen p-6 flex justify-center">
+            News 
+        </div>
+        <div>
+            Refresher Exam
+        </div>
+        </>
+    )
+}

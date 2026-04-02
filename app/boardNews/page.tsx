094431ff1a0cb98f7cb612384af8ed169dd5879f
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-02 11:06:15 -0400

    changed the min height screen

--- app/boardNews/page.tsx
+++ app/boardNews/page.tsx
@@ -1,7 +1,7 @@
 export default function BoardNews() {
     return (
         <>
-        <div className="bg-black min-h-screen p-6 flex justify-center">
+        <div className="bg-black p-6 flex justify-center">
             News 
         </div>
         <div>

583f5f8bce87e8498728f9783b171a345b406fd9
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-02 11:48:23 -0400

    refresher exam site page

--- app/boardNews/page.tsx
+++ app/boardNews/page.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
 export default function BoardNews() {
-    return (
-        <>
-        <div className="bg-black p-6 flex justify-center">
-            News 
-        </div>
-        <Link href="/boardHistory" className="bg-gray-800 text-white px-4 py-2 rounded-md mt-4">
-            Refresher Exam
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="bg-black p-6 flex justify-center">News</div>
+        <Link
+          href="/refresherExam"
+          className="bg-gray-800 text-white px-4 py-2 rounded-md mt-4"
+        >
+          Refresher Exam
         </Link>
-        </>
-    )
+      </div>
+    </>
+  );
 }

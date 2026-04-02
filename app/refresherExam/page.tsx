a1870c44dd18ad03256a632a8b47cba6ba50dabb
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-02 11:48:55 -0400

    page for refresher exam with link

---
+++ app/refresherExam/page.tsx
@@ -0,0 +1,29 @@
+export default function RefresherExam() {
+  return (
+    <>
+      <p>
+        The annual refresher exam for Active members is available via the
+        Documents section of this site. To complete the exam online,{" "}
+        <a
+          href="https://testmoz.com/2325811"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          click here
+        </a>
+        . Enter your name, click “continue” and the exam will open. Click Yes or
+        No for each question, and when you are done, click “submit.” You must
+        get a minimum of 43 correct to pass.
+      </p>
+      <p>
+        The deadline to complete the exam is Dec. 1 at 8 p.m. When you complete
+        it online, scoring is tabulated instantly, with the results immediately
+        communicated to you as well as the board secretary.
+      </p>
+      <p>
+        As always, the exam is open-book. Please contact Jeff Rivet if you
+        require further assistance.
+      </p>
+    </>
+  );
+}

abde84827ddec430eec85ae57af9df6d40290633
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-02 11:09:24 -0400

    added link component ti refresher

--- app/boardNews/page.tsx
+++ app/boardNews/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function BoardNews() {
     return (
         <>
         <div className="bg-black p-6 flex justify-center">
             News 
         </div>
-        <div>
+        <Link href="/boardHistory" className="bg-gray-800 text-white px-4 py-2 rounded-md mt-4">
             Refresher Exam
-        </div>
+        </Link>
         </>
     )
 }

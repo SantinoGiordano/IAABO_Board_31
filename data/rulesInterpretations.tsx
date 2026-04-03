98c0fd398f0b2cda64b6499a8773878994e91480
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-03 10:50:53 -0400

    temp data

--- data/rulesInterpretations.tsx
+++ data/rulesInterpretations.tsx
@@ -3,13 +3,13 @@ import { RuleInterpretation } from "@/types/type";
 
 export const rulesInterpretations: RuleInterpretation[] = [
   {
-    hostBoard: "Springfield Board 130",
+    hostBoard: "130",
     date: "April 10, 2026",
     time: "7:00 PM",
     location: "Springfield High School",
   },
   {
-    hostBoard: "Holyoke Board 129",
+    hostBoard: "129",
     date: "April 15, 2026",
     time: "6:30 PM",
     location: "Holyoke Community Center",

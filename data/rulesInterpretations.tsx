c2e9ded8089af51866e283fc11709b1c020da801
SantinoGiordano <santinogiordano13@gmail.com>
2026-04-03 10:46:55 -0400

    added data for rules interatation

---
+++ data/rulesInterpretations.tsx
@@ -0,0 +1,17 @@
+import { RuleInterpretation } from "@/types/type";
+
+
+export const rulesInterpretations: RuleInterpretation[] = [
+  {
+    hostBoard: "Springfield Board 130",
+    date: "April 10, 2026",
+    time: "7:00 PM",
+    location: "Springfield High School",
+  },
+  {
+    hostBoard: "Holyoke Board 129",
+    date: "April 15, 2026",
+    time: "6:30 PM",
+    location: "Holyoke Community Center",
+  },
+];
